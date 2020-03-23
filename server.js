01282c3e4608795bb88a82966483e090bab9ebef
Glitch (dyad) <none>
2020-03-23 20:43:46 +0000

    💰🍠day 57

--- server.js
+++ server.js
@@ -16,34 +16,50 @@ unity.on('connection', function(socket){
   console.log('unity connected');
 });
 
-
-//clients
+let players = 2;
+//clients 
+//so for now, both clients can use the same url
 var inputs = io.of('/input');
-inputs.on('connection', function(socket){
+inputs.on('connection', function(socket){  
+  console.log("players count = " + players);
   console.log('new input client!: ' + socket.id);
-
-  socket.on('spawn', function(spawnData){
-    console.log(spawnData.x + " : " + spawnData.y);
-    unity.emit('spawn', spawnData);
+  socket.on('player', function(){
+    //hacky fix to get 2p to work
+    if (players == 1){
+      players = 2;
+    }
+    else if (players == 2)
+    {
+      players = 1;
+    }
+    socket.emit('get player', {player: players});
   });
-  
-  // socket.on('log', function(data){
-  //   console.log("mX: " + data.mouseX + ", mY: " + data.mouseY);
-  // });
-//   //click
-//   socket.on('click', function(){
-//     console.log("click");
-//     unity.emit('click');
-//   });
-  
-//   //upgrade
-//   socket.on('upgrade', function(){
-//     console.log("upgrade");
-//     unity.emit('upgrade');
-//   });
+  socket.on('drop', function(_player){
+    let p = _player.player
+    // console.log(p);
+    console.log("player " + p + ": slappa/drop");
+    unity.emit('drop', _player);
+  });
+  socket.on('type', function(player){
+    console.log("player " + player + ": beskrivning/type");
+    unity.emit('type', player);
+  });
+  socket.on('rotate', function(player){
+    console.log("player " + player + ": vinkel/rotate");
+    unity.emit('rotate', player);
+  });
+  socket.on('scale', function(player){
+    console.log("player " + player + ": skalla/scale");
+    unity.emit('scale', player);
+  });
+  socket.on('move', function(player){
+    console.log("player " + player + ": plats/move");
+    unity.emit('move', player);
+  });
   
   socket.on('disconnect', function(){
     console.log('disconnected: ' + socket.id);
+    // players--;
     // unity.emit('clientDisconnect', {id: socket.id});
     // console.log('disconnect' + client_arrays[0].id);
     // for (let i = client_arrays.length - 1; i >= 0 ; i--){
@@ -55,6 +71,7 @@ inputs.on('connection', function(socket){
 });
 
 //2p
+/*
 var inputs2 = io.of('/input2');
 inputs2.on('connection', function(socket){
   console.log('new 2p client!: ' + socket.id);
@@ -63,21 +80,6 @@ inputs2.on('connection', function(socket){
     console.log("spawn 2" + spawnData.x + " : " + spawnData.y);
     unity.emit('spawn2', spawnData);
   });
-  
-  // socket.on('log', function(data){
-  //   console.log("mX: " + data.mouseX + ", mY: " + data.mouseY);
-  // });
-//   //click
-//   socket.on('click', function(){
-//     console.log("click");
-//     unity.emit('click');
-//   });
-  
-//   //upgrade
-//   socket.on('upgrade', function(){
-//     console.log("upgrade");
-//     unity.emit('upgrade');
-//   });
   
   socket.on('disconnect', function(){
     console.log('disconnected: ' + socket.id);
@@ -90,3 +92,4 @@ inputs2.on('connection', function(socket){
     // }
   });
 });
+*/
